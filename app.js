6bf1fa3f04aea97a2c88e8540755bd89a9a5b1b1
Furkan Durak <furkandurak22@hotmail.com>
2023-04-07 23:13:18 +0300

    first

---
+++ app.js
@@ -0,0 +1,26 @@
+let buttons = document.querySelectorAll("button");
+let screenDisplay = document.querySelector(".screen");
+
+let calculation =[];
+let accumulativeCalculation;
+let geval;
+
+function calculate(button) {
+    const value = button.textContent;
+    if(value === "CLEAR") {
+        calculation = [];
+        screenDisplay.textContent = ".";
+    } else if(value === "=") {
+        screenDisplay.textContent = eval(accumulativeCalculation);
+    } else {
+        calculation.push(value);
+        accumulativeCalculation = calculation.join("");
+        screenDisplay.textContent = accumulativeCalculation;
+    }
+    
+}
+
+
+
+
+buttons.forEach(button => button.addEventListener("click", ()=> calculate(button)));
